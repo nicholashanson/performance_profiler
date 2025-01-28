3e165d28deb00159bd3e1e79ad613b5ba88991f6
Nick <nicholashanson27@outlook.com>
2025-01-28 21:52:32 +0800

    test fixture variables

--- tests.cpp
+++ tests.cpp
@@ -1,27 +1,29 @@
 #include "performance_profile.hpp"
 
-struct f : profiler::fixture<1,1> {
+struct f : profiler::base_fixture<1,1> {
 
-    f() = default;
+    f() : counter(0) {
+    }
+
+    size_t counter;
 };
 
 PROFILE_AVG( use_macro, count_to_1000, f, {
 
     START_TIMER
-    size_t counter{};
     for ( size_t i = 0; i < 1000; ++i )
-        counter++;
+        fx.counter++;
     END_TIMER
 })
 
 const bool profile_initialized = profiler::profile_avg<f>(
     "use_function_template",
     "count_to_100",
-    [](auto& fx, auto& start_time, auto& end_time) {
+    []( auto& fx, auto& start_time, auto& end_time ) {
+        size_t c{};
         START_TIMER
-        size_t counter{};
         for ( size_t i = 0; i < 1000; ++i )
-            counter++;
+            fx.counter;
         END_TIMER
     }
 );
