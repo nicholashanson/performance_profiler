5f5f6c6917d293b9c856a21e5d6923f8118bdf22
Nick <nicholashanson27@outlook.com>
2025-01-28 21:53:29 +0800

    changed struct nme to base_fixture

--- fixture.hpp
+++ fixture.hpp
@@ -4,17 +4,17 @@
 namespace profiler {    
     
     template<size_t iterations = 1, size_t runs = 1>
-    struct fixture {
+    struct base_fixture {
 
         static size_t iterations_;
         static size_t runs_;
     };
 
     template<size_t iterations, size_t runs>
-    size_t fixture<iterations, runs>::iterations_ = iterations;
+    size_t base_fixture<iterations, runs>::iterations_ = iterations;
 
     template<size_t iterations, size_t runs>
-    size_t fixture<iterations, runs>::runs_ = runs;
+    size_t base_fixture<iterations, runs>::runs_ = runs;
 
 } // profiler
 
